8b4c5cc548d0461aae4b2fa95681049d046b9863
AG-M002 <shahzaibshakeelcheema@gmail.com>
2023-05-18 15:10:54 +0500

    App.js modified

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,8 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
+  const [courses, setCourses] = useState([]);
 
+  useEffect(() => {
+    // Fetch courses from backend
+    axios.get('http://localhost:5000/courses')
+      .then(res => {
+        setCourses(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>LMS School</h1>
+      <h2>Courses:</h2>
+      <ul>
+        {courses.map(course => (
+          <li key={course._id}>{course.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
